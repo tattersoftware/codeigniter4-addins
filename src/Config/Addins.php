db7711ba5d97ba8645afa5c8f5591f17956a5f3b
Matt Gatner <mgatner@tattersoftware.com>
2019-04-09 19:11:17 +0000

    Fix syntax errors

--- src/Config/Addins.php
+++ src/Config/Addins.php
@@ -21,57 +21,57 @@ class Addins extends BaseConfig
 				'scope'      => 'global',
 				'name'       => 'databaseTimezone',
 				'content'    => 'America/New_York',
-				'summary'    => 'Timezone for the database server(s)'
-				'protected'  => 1
+				'summary'    => 'Timezone for the database server(s)',
+				'protected'  => 1,
 			],
 			'perPage' => [
 				'scope'      => 'user',
 				'name'       => 'perPage',
 				'content'    => '10',
-				'summary'    => 'Number of items to show per page'
-				'protected'  => 1
+				'summary'    => 'Number of items to show per page',
+				'protected'  => 1,
 			],
 			'theme' => [
 				'scope'      => 'user',
 				'name'       => 'theme',
 				'content'    => 'Default',
-				'summary'    => 'Site theme to use'
-				'protected'  => 1
+				'summary'    => 'Site theme to use',
+				'protected'  => 1,
 			],
 			'siteTimezone' => [
 				'scope'      => 'global',
 				'name'       => 'serverTimezone',
 				'content'    => 'America/New_York',
-				'summary'    => 'Timezone for the web server(s)'
-				'protected'  => 1
+				'summary'    => 'Timezone for the web server(s)',
+				'protected'  => 1,
 			],
 			'siteVersion' => [
 				'scope'      => 'global',
 				'name'       => 'siteVersion',
 				'content'    => '1.0.0',
-				'summary'    => 'Current version of this project'
-				'protected'  => 1
+				'summary'    => 'Current version of this project',
+				'protected'  => 1,
 			],
 			'timezone' => [
 				'scope'      => 'user',
 				'name'       => 'timezone',
 				'content'    => 'America/New_York',
-				'summary'    => 'Timezone for the user'
-				'protected'  => 1
+				'summary'    => 'Timezone for the user',
+				'protected'  => 1,
 			],
 			'baseControllerHash' => [
 				'scope'      => 'global',
 				'name'       => 'baseControllerHash',
 				'content'    => '',
-				'summary'    => 'Hash of BaseController from tatter/addins (to check for updates)'
-				'protected'  => 0
+				'summary'    => 'Hash of BaseController from tatter/addins (to check for updates)',
+				'protected'  => 0,
 			],
 			'baseModelHash' => [
 				'scope'      => 'global',
 				'name'       => 'baseModelHash',
 				'content'    => '',
-				'summary'    => 'Hash of BaseModel from tatter/addins (to check for updates)'
-				'protected'  => 0
+				'summary'    => 'Hash of BaseModel from tatter/addins (to check for updates)',
+				'protected'  => 0,
 			],
 		];
 }
